0b9526f12594902d60db51f801326e1281780541
Patrick Collins <pat@burned.com>
2021-04-12 17:59:14 -0400

    mf68 changes

--- keyboards/40percentclub/mf68/keymaps/patcoll/keymap.c
+++ keyboards/40percentclub/mf68/keymaps/patcoll/keymap.c
@@ -7,6 +7,7 @@
 #define BSLS_FN LT(1,KC_BSLS)
 #define ESC_FN LT(2,KC_ESC)
 #define GRV_FN LT(2,KC_GRV)
+#define Q_FN LT(2,KC_Q)
 #define BR_BACK LALT(KC_LBRC)
 #define BR_FWD LALT(KC_RBRC)
 
@@ -40,24 +41,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [0] = LAYOUT_68_ansi(
-    GRV_FN,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_MPLY, KC_PGUP,
-    TAB_FN,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, BSLS_FN,          KC_DEL,  KC_PGDN,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_MPLY, KC_PGUP,
+    TAB_FN,  Q_FN,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, BSLS_FN,          KC_DEL,  KC_PGDN,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT,          KC_UP,
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   MO(2),            KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_LALT, KC_LGUI,   KC_LCTL,          KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [1] = LAYOUT_68_ansi(
-    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_VOLD, KC_VOLU, KC_MPLY,          _______, KC_VOLU,
-    _______, _______, KC_UP,   _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_END,  BR_BACK, BR_FWD,  KC_PSCR,          _______, KC_VOLD,
+    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,          _______, KC_VOLU,
+    _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, BR_BACK, BR_FWD,  KC_PSCR,          _______, KC_VOLD,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,          _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,          KC_MUTE,
+    _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,                   _______,          KC_MUTE,
     _______, _______, _______,                            _______,                            _______, _______, _______,          KC_MPRV, KC_MPLY, KC_MNXT
   ),
   [2] = LAYOUT_68_ansi(
-    _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-    _______, _______, KC_MS_U, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______, _______,          _______, _______,
+    _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU,
+    _______, _______, KC_MS_U, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLD,
     _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,          _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_BTN1,          KC_MS_U,
+    _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,                   KC_BTN1,          KC_MS_U,
     _______, _______, _______,                            KC_BTN1,                            _______, _______, KC_BTN1,          KC_MS_L, KC_MS_D, KC_MS_R
   )
 };
