cb7e35a320a61626b81ba2361fc2b8d597bc55a6
Patrick Collins <pat@burned.com>
2021-05-10 16:22:40 -0400

    nyquist

--- keyboards/keebio/nyquist/keymaps/patcoll/keymap.c
+++ keyboards/keebio/nyquist/keymaps/patcoll/keymap.c
@@ -56,24 +56,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LALT_T(KC_GRV),   KC_1,             KC_2,    KC_3,                 KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   LT(_NAV, KC_TAB), LT(_MOUSE, KC_Q), KC_W,    KC_E,                 KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   LCTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,                 KC_G,    KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,                 KC_B,    KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LSFT_T(KC_ENT),
-  KC_LSFT,          KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_SPC),   KC_SPC,  LCTL_T(KC_ENT), LT(_RAISE, KC_BSPC),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,                 KC_B,    KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_UP, LSFT_T(KC_SLSH),
+  KC_LSFT,          KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_SPC),   KC_SPC,  LCTL_T(KC_ENT), LT(_RAISE, KC_BSPC),   KC_LALT, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
 [_LOWER] = LAYOUT(
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PSCR,
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, KC_MUTE,
-  _______, _______, _______, _______, _______, _______, KC_LCTL, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+  KC_LCTL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, KC_PGUP, _______,
+  RESET,   _______, _______, _______, _______, _______, KC_LCTL, _______, _______, KC_HOME, KC_PGDN, KC_END
 ),
 
 [_RAISE] = LAYOUT(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_MUTE,
-  _______, _______, _______, _______, _______, _______, KC_LCTL, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_LCTL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, KC_BRIU, _______,
+  RESET,   _______, _______, _______, _______, _______, KC_LCTL, _______, _______, _______, KC_BRID, _______
 ),
 
 [_ADJUST] = LAYOUT(
@@ -89,16 +89,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-  _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
-  _______, _______, _______, _______, _______, _______, KC_LCTL, _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_VOLU, _______,
+  _______, _______, _______, _______, _______, _______, KC_LCTL, _______, _______, KC_MNXT, KC_VOLD, KC_MPLY
 ),
 
 [_MOUSE] = LAYOUT(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,
   _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-  _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
-  _______, _______, _______, _______, _______, KC_BTN1, KC_BTN1, KC_BTN3, KC_BTN2, _______, _______, _______
+  _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_MS_U, _______,
+  _______, _______, _______, _______, _______, KC_BTN1, KC_BTN1, KC_BTN3, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R
 ),
 
 };
