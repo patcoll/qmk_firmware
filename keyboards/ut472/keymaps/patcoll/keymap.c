c486816586c56496b7080297f92791d59b0306b6
Patrick Collins <pat@burned.com>
2021-06-10 12:16:31 -0400

    Add GUI key instead

--- keyboards/ut472/keymaps/patcoll/keymap.c
+++ keyboards/ut472/keymaps/patcoll/keymap.c
@@ -16,7 +16,7 @@ LAYOUT(
   LT3_TAB, LT4_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   SFT_SLSH,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, LT2_SPC,     KC_SPC,       LT1_ENT, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT
+  KC_LCTL, KC_LGUI, KC_LGUI, KC_LALT, LT2_SPC,     KC_SPC,       LT1_ENT, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 LAYOUT( /* Right */
